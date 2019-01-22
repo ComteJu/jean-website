c9acd08be137eb1c6b142e61b862644c16a6dead
Ja Comte <lcmte.ju@gmail.com>
2019-01-22 21:51:38 +0100

    add seo image

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -19,13 +19,16 @@ export default class SEO extends Component {
               defaultDescription,
               defaultBanner
             }
+          },
+          image: {
+            image_url
           }
         }) => {
           const seo = {
             title: title || defaultTitle,
             description: defaultDescription || desc,
             image: `${siteUrl}${banner || defaultBanner}`,
-            url: `${siteUrl}${pathname || "/"}`
+            url: `${siteUrl}${pathname || "/"}`,
           };
 
           let schemaOrgJSONLD = [
@@ -33,30 +36,31 @@ export default class SEO extends Component {
               "@context": "http://schema.org",
               "@type": "WebSite",
               "@id": siteUrl,
-              url: siteUrl,
-              name: defaultTitle,
-              alternateName: titleAlt || ""
+              "url": siteUrl,
+              "name": defaultTitle,
+              "alternateName": titleAlt || "",
+              "image": image_url
             }
           ];
 
           return (
-              <Helmet title={seo.title}>
-                <html lang={siteLanguage} />
-                <meta name="description" content={seo.description} />
-                <meta name="image" content={seo.image} />
-                <meta name="apple-mobile-web-app-title" content={shortName} />
-                <meta name="application-name" content={shortName} />
-                <script type="application/ld+json">
-                  {JSON.stringify(schemaOrgJSONLD)}
-                </script>
+            <Helmet title={seo.title}>
+              <html lang={siteLanguage} />
+              <meta name="description" content={seo.description} />
+              <meta name="image" content={seo.image} />
+              <meta name="apple-mobile-web-app-title" content={shortName} />
+              <meta name="application-name" content={shortName} />
+              <script type="application/ld+json">
+                {JSON.stringify(schemaOrgJSONLD)}
+              </script>
 
-                <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-                <meta
-                  content="width=device-width,initial-scale=1.0,user-scalable=yes"
-                  name="viewport"
-                />
-                <meta content="yes" name="apple-mobile-web-app-capable" />
-              </Helmet>
+              <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+              <meta
+                content="width=device-width,initial-scale=1.0,user-scalable=yes"
+                name="viewport"
+              />
+              <meta content="yes" name="apple-mobile-web-app-capable" />
+            </Helmet>
           );
         }}
       />
@@ -87,6 +91,9 @@ const query = graphql`
         defaultDescription: description
         defaultBanner: banner
       }
+    }
+    image: trelloCard(name: { eq: "La maison des peintres" }) {
+      image_url
     }
   }
 `;
